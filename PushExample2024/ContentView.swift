93a155b5c1c18a474274f34afc448b63341c27bf
Sulayman Md <sulayman.md@liebherr.com>
2024-01-16 15:50:14 +0100

    Initial Commit

---
+++ PushExample2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushExample2024
+//
+//  Created by Sulayman Md on 16.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushExample2024/PushExample2024App.swift
@@ -0,0 +1,17 @@
+//
+//  PushExample2024App.swift
+//  PushExample2024
+//
+//  Created by Sulayman Md on 16.01.24.
+//
+
+import SwiftUI
+
+@main
+struct PushExample2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
